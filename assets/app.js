8ae48bc529fc4cba68ebb451c1afae7ad82aed48
Gian <giangravina@gmail.com>
2020-08-26 12:02:02 -0400

    added more js features

--- assets/app.js
+++ assets/app.js
@@ -34,20 +34,45 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         //next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        //toggle active class css
-        
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        //remove dice on active toggle
-
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
 
     }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+        // add current score to global score
+        scores[activePlayer] += roundScore;
+
+        // update UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+
+        // check if player won the game
+        if (scores[activePlayer] >= 20) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        } else {
+        //next player
+        nextPlayer();
+        }
+});
+
+function nextPlayer(){
+     //next player
+     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+     roundScore = 0;
+
+     document.getElementById('current-0').textContent = '0';
+     document.getElementById('current-1').textContent = '0';
+
+     //toggle active class css
+     
+     document.querySelector('.player-0-panel').classList.toggle('active');
+     document.querySelector('.player-1-panel').classList.toggle('active');
+
+     //remove dice on active toggle
+
+     document.querySelector('.dice').style.display = 'none';
+}
